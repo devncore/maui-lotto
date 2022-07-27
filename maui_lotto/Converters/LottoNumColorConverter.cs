449c38fa91b9808fbd0e5ba2cfde1fc44b039980
EdisonKode <edisonkode@naver.com>
2022-07-28 08:48:58 +0900

    Update

--- maui_lotto/Converters/LottoNumColorConverter.cs
+++ maui_lotto/Converters/LottoNumColorConverter.cs
@@ -14,15 +14,15 @@ namespace maui_lotto.Converters
             Brush brush = Colors.White;
             int num = int.Parse(value.ToString());
             if(num <= 10)
-                brush = Color.FromHex("#FBC400");
+                brush = Color.FromArgb("#FBC400");
             else if (num <= 20)
-                brush = Color.FromHex("#1E47D6");
+                brush = Color.FromArgb("#1E47D6");
             else if (num <= 30)
-                brush = Color.FromHex("#FF7272");
+                brush = Color.FromArgb("#FF7272");
             else if (num <= 40)
-                brush = Color.FromHex("#1E1E1E");
+                brush = Color.FromArgb("#1E1E1E");
             else
-                brush = Color.FromHex("#55B155");
+                brush = Color.FromArgb("#55B155");
 
             return brush;
         }
--- maui_lotto/ViewModels/ResultViewModel.cs
+++ maui_lotto/ViewModels/ResultViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using maui_lotto.Models;
+using System.Windows.Input;
 using VijayAnand.MauiToolkit.Core.Services;
 using VijayAnand.MauiToolkit.Services;
 using VijayAnand.Toolkit.ObjectModel;
@@ -11,17 +12,71 @@ namespace maui_lotto.ViewModels
     {
         readonly IDialogService? dialogService;
 
+        public ICommand LoadCommand { get; protected set; }
+
         public ResultViewModel()
         {
             Title = "당첨결과";
             dialogService = AppService.GetService<IDialogService>();
-
-            resultList = lottoResult.OrderByDescending(x => x.drwNo).ToList();
+            LoadCommand = new AsyncRelayCommand(async () =>
+            {
+                await Task.Delay(500);
+                Initialize();                
+            });
         }
 
 
-        [ObservableProperty]
-        private List<LottoResult> resultList;
+        void Initialize()
+        {
+            var list = lottoResult.OrderByDescending(x => x.drwNo).ToList();
+
+            int count = 0;
+            foreach(var row in list)
+            {
+                row.color1 = LottoNumToColorConvert(row.drwtNo1);
+                row.color2 = LottoNumToColorConvert(row.drwtNo2);
+                row.color3 = LottoNumToColorConvert(row.drwtNo3);
+                row.color4 = LottoNumToColorConvert(row.drwtNo4);
+                row.color5 = LottoNumToColorConvert(row.drwtNo5);
+                row.color6 = LottoNumToColorConvert(row.drwtNo6);
+                row.colorBonus = LottoNumToColorConvert(row.bnusNo);
+
+                if (count % 2 == 0)
+                    row.bgColor = Color.FromArgb("#D3D3D3");
+                else
+                    row.bgColor = Colors.White;
+
+                count++;
+            }
+
+            ResultList = list;
+        }
+
+        Brush LottoNumToColorConvert(int num)
+        {
+            Brush brush = Colors.White;
+            if (num <= 10)
+                brush = Color.FromArgb("#FBC400");
+            else if (num <= 20)
+                brush = Color.FromArgb("#1E47D6");
+            else if (num <= 30)
+                brush = Color.FromArgb("#FF7272");
+            else if (num <= 40)
+                brush = Color.FromArgb("#1E1E1E");
+            else
+                brush = Color.FromArgb("#55B155");
+
+            return brush;
+        }
+
+
+        private static List<LottoResult> _resultList;
+        public List<LottoResult> ResultList
+        {
+            get { return _resultList; }
+            set { _resultList = value; OnPropertyChanged(); }
+        }
+
 
         [RelayCommand]
         async void Test()
